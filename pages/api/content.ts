87cf63da775fa0fd9843a0e07bcce4df7d2902b2
Zaqpurpur-Neo <baristaunescio@gmail.com>
2025-10-01 15:00:57 +0700

    add cors handler for now

--- pages/api/content.ts
+++ pages/api/content.ts
@@ -8,6 +8,14 @@ async function handler(
 ) {
 	res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate=300");
 
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+	if(req.method === "OPTIONS") {
+		return res.status(200).end()
+	}
+
 	if(req.method !== "GET") return res.status(405).json({ error: "Method not allowed" });
 	if(req.headers.authorization !== `Bearer ${process.env.ADMIN_KEY}`) {
 		return res.status(401).json({ error: "Unauthorized" })
--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -6,6 +6,14 @@ async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<any>
 ) {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+	if(req.method === "OPTIONS") {
+		return res.status(200).end()
+	}
+
 	if(req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
 	if(req.headers.authorization !== `Bearer ${process.env.ADMIN_KEY}`) {
 		return res.status(401).json({ error: "Unauthorized" })
